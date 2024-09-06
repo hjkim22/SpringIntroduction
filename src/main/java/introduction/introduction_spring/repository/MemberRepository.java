5a707fd719097693fe58d08da9f503c63ccd0bf4
hjkim <hjhjgudwls1022@gmail.com>
2024-09-06 22:15:27 +0900

    feat: 회원 서비스 개발/테스트

---
+++ src/main/java/introduction/introduction_spring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package introduction.introduction_spring.repository;
+
+import introduction.introduction_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
